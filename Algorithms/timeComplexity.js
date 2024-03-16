72e780e50b41d12343a31154adc2af6d73087f51
md musharruf <mdmusharruf13@gmail.com>
2024-03-16 07:30:52 +0530

    theory

---
+++ Algorithms/timeComplexity.js
@@ -0,0 +1,24 @@
+/**
+ * Time Complexity of Objects and Arrays
+ * 
+ * 
+ * An object is a collection of key value pairs
+ * Insert - O(1)
+ * Remove - O(1)
+ * Access - O(1)
+ * Search - O(n)
+ * Object.keys() - O(n)
+ * Object.values() - O(n)
+ * Object.entries() - O(n)
+ * 
+ * 
+ * 
+ * An array is an ordered collection of values.
+ * Insert / Remove at end - O(1)
+ * Insert / Remove at beginning - O(1)
+ * Access - O(1)
+ * Search - O(n)
+ * Push / Pop -O(1)
+ * shift / unshift / concat / slice / splice - O(n)
+ * forEach / map / filter / reduce - O(n)
+ */
