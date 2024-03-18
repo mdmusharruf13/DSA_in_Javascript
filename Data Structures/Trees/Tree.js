6049f469105a509fdc38628718fe0c35c97e677c
md musharruf <mdmusharruf13@gmail.com>
2024-03-19 00:48:22 +0530

    tree theory

---
+++ Data Structures/Trees/Tree.js
@@ -0,0 +1,7 @@
+/**
+ * A tree is a hierarchial data structure that consist of nodes connected by edges.
+ * A tree is a non linear ds, conpared to arrays, linked list, stack and queue which are linear ds.
+ * In linear ds, the time req. to search is proportional to the size of the data set.
+ * Tree however, woing to the nonlinear nature allow quicker and easier to the data.
+ * A tree will not contain any loop or cycles
+ */
