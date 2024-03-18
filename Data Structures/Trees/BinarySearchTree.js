9f7e765db005c9b76fca9b99f617f4ecad916af5
md musharruf <mdmusharruf13@gmail.com>
2024-03-19 00:49:49 +0530

    insert node fn

---
+++ Data Structures/Trees/BinarySearchTree.js
@@ -0,0 +1,72 @@
+/**
+ * A binary tree is a tree DS in which each node has atmost two children.
+ * They are referred to as left child and right child.
+ * 
+ * BST operations:
+ * insertion - To add a node to the tree.
+ * search - To find a node given its value.
+ * DFS & BFS - To visit all nodes in the tree.
+ * Deletion - To remove a node from the tree.
+ * 
+ * Usage:
+ * searching
+ * sorting
+ * to implement ADT such as lookup takles and priority queue.
+ */
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    isEmpty() {
+        return this.root === null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.isEmpty()) {
+            this.root = newNode;
+        }
+        else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode) {
+        if (newNode.value < root.value) {
+            if (root.left === null) {
+                root.left = newNode;
+            }
+            else {
+                this.insertNode(root.left, newNode);
+            }
+        }
+        else {
+            if (root.right === null) {
+                root.right = newNode;
+            }
+            else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+
+
+
+}
+
+
+const bst = new BST();
+console.log(bst.isEmpty());
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
