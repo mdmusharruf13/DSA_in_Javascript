e9258e00e7595f13c0d0dbfd7aed607ae0350cb7
md musharruf <mdmusharruf13@gmail.com>
2024-03-19 01:29:38 +0530

    traversal bfs and dfs

--- Data Structures/Trees/BinarySearchTree.js
+++ Data Structures/Trees/BinarySearchTree.js
@@ -78,6 +78,46 @@ class BST {
         }
     }
 
+    preorder(node) {
+        if (!node) {
+            return;
+        }
+        console.log(node.value);
+        this.preorder(node.left);
+        this.preorder(node.right);
+    }
+    inorder(node) {
+        if (!node) {
+            return;
+        }
+        this.inorder(node.left);
+        console.log(node.value);
+        this.inorder(node.right);
+    }
+    postorder(node) {
+        if (!node) {
+            return;
+        }
+        this.postorder(node.left);
+        this.postorder(node.right);
+        console.log(node.value);
+    }
+
+    levelOrder() {
+        const queue = [];
+        queue.push(this.root);
+        while (queue.length) {
+            let curr = queue.shift();
+            console.log(curr.value);
+            if (curr.left) {
+                queue.push(curr.left);
+            }
+            if (curr.right) {
+                queue.push(curr.right);
+            }
+        }
+    }
+
 }
 
 
@@ -86,6 +126,19 @@ console.log(bst.isEmpty());
 bst.insert(10);
 bst.insert(5);
 bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
 
 console.log(bst.search(bst.root, 50));
 console.log(bst.search(bst.root, 5));
+
+console.log("preorder");
+bst.preorder(bst.root);
+console.log("inorder");
+bst.inorder(bst.root);
+console.log("postorder");
+bst.postorder(bst.root);
+
+console.log("level order");
+bst.levelOrder(bst.root);
--- Data Structures/Trees/Tree.js
+++ Data Structures/Trees/Tree.js
@@ -4,4 +4,13 @@
  * In linear ds, the time req. to search is proportional to the size of the data set.
  * Tree however, woing to the nonlinear nature allow quicker and easier to the data.
  * A tree will not contain any loop or cycles
+ * 
+ * Traversals:
+ * Depth first search :
+ * The DFS  algo starts at the root node and explores as far as possible along each branch before backtracking
+ * Visit the root node, visit all the nodes in the left sub tree and visit all the nodes in the right sub tree.
+ * Depending on the order in which we do this, there can be three types of DFS traversal:
+ * 1. Preorder
+ * 2. Inorder
+ * 3. Postorder 
  */
